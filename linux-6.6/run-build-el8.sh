5c5644041689b6cd3ddf50b3a7514c2bbefa57a9
Kenneth Davidson <xylene2016@protonmail.com>
2024-10-07 11:47:14 -0400

    Update run-build-el8.sh

--- linux-6.6/run-build-el8.sh
+++ linux-6.6/run-build-el8.sh
@@ -10,6 +10,7 @@ docker run --cpus="2.0" --hostname 'x86-040.build.eng.bos.redhat.com' --rm --nam
 sleep 2
 docker exec al8 yum clean all
 docker exec al8 yum makecache
+docker exec al8 yum update -y
 docker exec al8 /bin/bash -c 'rm -fr /tmp/*'
 docker cp el8 al8:/home/
 docker exec al8 /bin/bash /home/el8/00-run-all.sh
