eb5934d738ca3bd0d934a0beaf7b984eb71dd96b
Kenneth Davidson <xylene2016@protonmail.com>
2024-10-07 12:47:31 -0400

    Update 00-run-all.sh

--- linux-6.6/el8/00-run-all.sh
+++ linux-6.6/el8/00-run-all.sh
@@ -8,7 +8,7 @@ bash 00-update_linux-firmware.sh
 bash 99-clean.sh
 bash 01-dl-linux-X.Y.sh "6.6"
 bash 02-setup.sh
-bash 03-run.sh || tail -n 100 /tmp/make_rpm-pkg.log
+bash 03-run.sh || tail -n 200 /tmp/make_rpm-pkg.log
 echo ; grep '^CONFIG_' /tmp/make_rpm-pkg.log || : ; echo
 tail -n 7 /tmp/make_rpm-pkg.log
 bash 04-copy.sh
