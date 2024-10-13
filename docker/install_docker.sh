047ccd3a228e9d946d4c7c2f72f83fc20ab2604c
Kenneth Davidson <xylene2016@protonmail.com>
2024-10-12 23:47:32 -0400

    Update install_docker.sh

--- docker/install_docker.sh
+++ docker/install_docker.sh
@@ -13,9 +13,9 @@ set -e
 _tmp_dir="$(mktemp -d)"
 cd "${_tmp_dir}"
 wget -q -c -t 9 -T 9 \
-"https://github.com/icebluey/docker/releases/download/v2024-09-25/containerd-1.7.22-1_amd64.tar.xz"
+"https://github.com/icebluey/docker/releases/download/v2024-10-13/containerd-1.7.22-1_amd64.tar.xz"
 wget -q -c -t 9 -T 9 \
-"https://github.com/icebluey/docker/releases/download/v2024-09-25/docker-only-27.3.1-1_amd64.tar.xz"
+"https://github.com/icebluey/docker/releases/download/v2024-10-13/docker-only-27.3.1-1_amd64.tar.xz"
 rm -f /usr/bin/containerd
 rm -fr /usr/bin/containerd-*
 rm -f /usr/bin/ctr
